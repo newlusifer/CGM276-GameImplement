d97fb4ec8b5e3ef3b6f3b7a7fa2841d1bf5ef150
unknown <newlusiferkp@gmail.com>
2019-01-20 10:57:13 +0700

    test

--- myApp1/Program.cs
+++ myApp1/Program.cs
@@ -6,7 +6,7 @@ namespace myApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Go GO GO");
         }
     }
 }
